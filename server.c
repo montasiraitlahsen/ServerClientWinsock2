61a0ab413e3b1d9b81ffba57c1bc651d5c040553
montacir ait lahcen <montaciraitlahcen@gmail.com>
2025-05-19 22:02:27 +0100

    added inbox and also general chat

--- server.c
+++ server.c
@@ -14,12 +14,20 @@ typedef struct
     SOCKET Clients;
 }Clients;
 int UsernameCount;
-Clients User[100];
+typedef struct
+{
+    Clients User[100];
+    char Recipient[100];
+    bool GeneralPrivate;
+}Into;
+Into Message;
 int Counter=0;
 // for receiving and printing message from teh client
 unsigned __stdcall ReceivingAndPrintingData(void *param);
 int main()
 {
+    Message.GeneralPrivate = FALSE;
+
     WSADATA wsaData;
     int result = WSAStartup(MAKEWORD(2, 2), &wsaData);
     if (result != 0) {
@@ -82,66 +90,82 @@ unsigned __stdcall ReceivingAndPrintingData(void *param)
     }
     if(resultnumber > 0)
     {
-        Username[resultnumber]='\0';
         for(int i=0;i<Counter;i++)
         {
-            if(strcmp(User[i].Username,Username)==0)
+            if(strcmp(Message.User[i].Username,Username)==0)
             {
                 Check=TRUE;
             }
         }
         if(!Check)
         {
-            User[Counter].Clients=*(SOCKET *)param;
-            strcpy(User[Counter++].Username,Username);
+            Message.User[Counter].Clients=*(SOCKET *)param;
+            strcpy(Message.User[Counter++].Username,Username);
             UsernameCount++;
         }
     }
+    Message.GeneralPrivate = TRUE;
     while(TRUE)
-    {   
+    {  
+        memset(Message.Recipient,0,sizeof(Message.Recipient));
+        int resultnumberb = recv(*(SOCKET*)param,Message.Recipient,sizeof(Message.Recipient),0);
+        int lenr = strlen(Message.Recipient);
+        while (lenr > 0 && (Message.Recipient[lenr-1] == '\n' || Message.Recipient[lenr-1] == '\r' || 
+        Message.Recipient[lenr-1] == '\t' || Message.Recipient[lenr-1] == ' '))
+        {
+            Message.Recipient[--lenr] = '\0';
+        }
+        if(strcmp(Message.Recipient," ") == 0)
+        {
+            Message.GeneralPrivate = FALSE;
+        }
         memset(Buffer,0,sizeof(Buffer));
         int resultnumber = recv(*(SOCKET*)param,Buffer,sizeof(Buffer),0);
+        int len = strlen(Buffer);
+        while (len > 0 && (Buffer[len-1] == '\n' || Buffer[len-1] == '\r' || 
+        Buffer[len-1] == '\t' || Buffer[len-1] == ' '))
+        {
+            Buffer[--len] = '\0';
+        }
         if(resultnumber>0)
         {
-            int len = strlen(Buffer);
-            while (len > 0 && (Buffer[len-1] == '\n' || Buffer[len-1] == '\r' || 
-            Buffer[len-1] == '\t' || Buffer[len-1] == ' '))
+            if(!Message.GeneralPrivate || (strcmp(Message.Recipient,"all") == 0))
             {
-                Buffer[--len] = '\0';
-            }
-            Buffer[resultnumber] = '\0';
-            if(strcmp(Buffer,"quit")==0)
-            {
-                break;
-            }
-            else
-            {
-                int len = strlen(Buffer);
-                while (len > 0 && (Buffer[len-1] == '\n' || Buffer[len-1] == '\r' || 
-                Buffer[len-1] == '\t' || Buffer[len-1] == ' '))
-                {
-                    Buffer[--len] = '\0';
-                }
                 for(int i=0;i<Counter;i++)
                 {
-                    if(User[i].Clients==*(SOCKET *)param)
+                    if(Message.User[i].Clients==*(SOCKET *)param)
                     {
-                        printf("-->%s says : %s\n",User[i].Username,Buffer);
                         for(int i=0;i<Counter;i++)
                         {
-                            if(User[i].Clients!=*(SOCKET *)param)
+                            if(Message.User[i].Clients!=*(SOCKET *)param)
                             {
-                                send(User[i].Clients,Username,strlen(Username),0);
-                                send(User[i].Clients,Buffer,strlen(Buffer),0);
+                                send(Message.User[i].Clients,Username,strlen(Username),0);
+                                send(Message.User[i].Clients,Buffer,strlen(Buffer),0);
                             }
                         }
                     }
                 }
             }
-        }
-        else
-        {
-            break;
+            else
+            {
+                int  searching = 0;
+                for(int i=0;i<Counter;i++)
+                {
+                    if(strcmp(Message.Recipient,Message.User[i].Username) == 0)
+                    {
+                        send(Message.User[i].Clients,Username,strlen(Username),0);
+                        send(Message.User[i].Clients,Buffer,strlen(Buffer),0);
+                    }
+                    else
+                    {
+                        searching++;                        
+                    }
+                }
+                if(searching == Counter)
+                {
+                    printf("the recipient are not found \n");
+                }
+            }
         }
     }
     closesocket(*(SOCKET*)param);
